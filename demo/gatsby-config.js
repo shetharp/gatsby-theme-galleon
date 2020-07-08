c5f0f5b8d1aef5eed676ed2d1f8233543d56e4fd
Arpit Sheth <shetharp@gmail.com>
2020-07-07 23:30:12 -0400

    Configure placeholders for parent theme

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,3 +1,82 @@
 module.exports = {
-  plugins: [{ resolve: `@shetharp/gatsby-theme-polaroid`, options: {} }],
+  siteMetadata: {
+    // Used for the title template on pages other than the index site
+    siteTitle: `Polaroid`,
+    // Default title of the page
+    siteTitleAlt: `Site Title Alt`, // TODO
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Site Headline`, // TODO
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://arpitsheth.com`,
+    // Used for SEO
+    siteDescription: `Site description`, // TODO
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`, // TODO
+    // Twitter Handle
+    author: `@shetharp`, // TODO
+  },
+  plugins: [
+    {
+      resolve: `@shetharp/gatsby-theme-polaroid`,
+      options: {
+        // TODO -- Add more options if needed
+        // https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog#theme-options
+
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/shetharp`,
+          },
+          {
+            name: `Instagram`,
+            url: `https://www.instagram.com/shetharp/`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID, // TODO
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Manifest Name`, // TODO
+        short_name: `short-name`, // TODO
+        description: `Manifest description`, // TODO
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`, // TODO -- should ideally pull from theme
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,5 +1,18 @@
-module.exports = {
-    plugins: [
-        `@lekoarts/gatsby-theme-minimal-blog`
-    ]
-}
+function GatsbyConfig(options) {
+    // TODO -- some of these custom options should be defined in the demo site instead...
+    const customOptions = {
+        ...options,
+        feed: true,
+        feedTitle: "TODO Feed Title"
+    }
+
+    return {
+        plugins: [
+            {
+                resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+                options: customOptions
+            }
+        ]
+    }
+};
+module.exports = GatsbyConfig;
